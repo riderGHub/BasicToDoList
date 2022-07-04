12e40ab11d717b92fd7f26a62de46f799171d661
rider6253 <patelsumit.1652002@gmail.com>
2022-07-04 16:13:05 +0530

    modified

--- src/todolistsbasic/App.jsx
+++ src/todolistsbasic/App.jsx
@@ -2,12 +2,15 @@
 ///////////////////////////////////////making TODOLISTs/////////////////
 import React from 'react'
 import Listpage from './Listpage'
+import "../index.css"
 
 const App=()=>{
 
    return(
     <>
+    <div className='pageBody'>
     <Listpage />
+    </div>
     </>
    )    
 
--- src/todolistsbasic/Listpage.jsx
+++ src/todolistsbasic/Listpage.jsx
@@ -1,51 +1,100 @@
 import React, { useState } from "react";
 import ToDolists from "./toDolists";
 import "../index.css";
-import { AddCircle } from "@material-ui/icons";
+
 
 const Listpage = () => {
+  const [len,setlen]=useState("🤔😃");
   const [inputlist, setinput] = useState([]);
   const [itm, enter_val] = useState("");
-  const Item = (e) => {
-    enter_val(e.target.value);
-  };
   const Add = () => {
-    console.log(inputlist);
     if (itm === "") return;
     setinput([...inputlist, itm]);
+////// changing Reactions /////////
+    var rxn=inputlist.length;
+    console.log(rxn);
+    if(rxn<3)
+    setlen("🤔😅");
+    else if(rxn<6)
+    setlen("🤔😟");
+    else if(rxn<10)
+    setlen("🤔🤕");
+    else if(rxn<13)
+    setlen("🤔🥲");
+    else if(rxn<16)
+    setlen("🤔😵‍💫");
+    else if(rxn<19)
+    setlen("🤔😨");
+    else 
+    setlen("🤔🤒🤯");
     enter_val("");
+  ///////////////////////////////
+    
   };
-
+  
+  const Item = (e) => {
+      enter_val(e.target.value);
+  };
   const del = (id) => {
     console.log("deleted");
+    /////////changing reactions/////////////
+    var rxn=inputlist.length;
+    if(rxn<2)setlen("🤔😃")
+    else if(rxn<5)
+    setlen("🤔😅");
+    else if(rxn<8)
+    setlen("🤔😟");
+    else if(rxn<12)
+    setlen("🤔🤕");
+    else if(rxn<15)
+    setlen("🤔🥲");
+    else if(rxn<18)
+    setlen("🤔😵‍💫");
+    else if(rxn<21)
+    setlen("🤔😨");
+    else 
+    setlen("🤔🤒🤯");
+   ///////////////////////////////////
     setinput((inputlist) => {
       return inputlist.filter((arrElem, indx) => {
         return indx !== id;
       });
     });
+    
   };
 
+  const check=(e)=>{
+      if(e.key==='Enter')Add();
+      }
+  
   const clear = () => {
     setinput([]);
+    //////Reaction/////////
+      setlen("🤔😃");   
+    ///////////////////////
   };
 
+  
+  
   return (
     <>
       <div className="main_div">
-        <div className="headline">ToDo List</div>
+        <div className="headline">Today's Task</div>
         <div className="inputfield">
           <input
             className="cin"
             type="text"
             placeholder="Add a Item"
             onChange={Item}
+            onKeyUp={check}
             value={itm}
           />
 
           <button className="addbtn" onClick={Add} title="Add to list">
             <i class="fas fa-solid fa-plus"></i>
-            
           </button>
+        </div>
+      <div className="emoji"> {len}
         </div>
         <ol>
           {inputlist.map((itmval, indx) => {
